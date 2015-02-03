c631a194278ccb757172f6247c005b15beded7c3
aeon <goldmine@163.com>
2015-02-03 11:03:44 +0800

    add title function

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -4,9 +4,14 @@ feature 'Creating Projects' do
   scenario "can create a project" do
     visit '/'
     click_link 'New Project'
-    fill_in 'Name', with: 'TextMate 2'
+    fill_in 'Name', with: "TextMate 2"
     fill_in 'Description', with: 'A text-editor for OS X'
     click_button 'Create Project'
     expect(page).to have_content('Project was successfully created.')
+
+    project = Project.where(name: 'TextMate 2').first
+    expect(page.current_url).to eql(project_url(project))
+    title = "TextMate 2 - Projects - Ticketee"
+    expect(page).to have_title(title)
   end
 end
