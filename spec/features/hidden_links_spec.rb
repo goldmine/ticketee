32477b9498f95e7ba58bd7760a91bc30636d13a6
aeon <goldmine@163.com>
2015-02-13 15:33:52 +0800

    restrick tickets operation

--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 feature "hidden links" do
-  let(:user) { FactoryGirl.create(:user) }
-  let(:admin) { FactoryGirl.create(:admin_user) }
-  let(:project) { FactoryGirl.create(:project) }
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:admin) { FactoryGirl.create(:admin_user) }
+  let!(:project) { FactoryGirl.create(:project) }
+  let!(:ticket) { FactoryGirl.create(:ticket, project: project, user: user) }
 
   context "anonymous users" do
     scenario "can not see the new project link" do
@@ -34,6 +35,43 @@ feature "hidden links" do
       visit project_path(project)
       expect(page).to_not have_link("Delete Project")
     end
+    scenario "New ticket link is shown to a user with permission" do
+      define_permission(user,'view', project)
+      define_permission(user,'create', project)
+      visit project_path(project)
+      expect(page).to have_link("New Ticket")
+    end
+    scenario "New ticket link is not shown to a user with permission" do
+      define_permission(user,'view', project)
+      visit project_path(project)
+      expect(page).to_not have_link("New Ticket")
+    end
+    scenario "edit ticket link is shown to a user with permission" do
+      define_permission(user,'view', project)
+      define_permission(user,'edit ticket', project)
+      visit project_path(project)
+      click_link ticket.title
+      expect(page).to have_link("Edit Ticket")
+    end
+    scenario "edit ticket link is not shown to a user with permission" do
+      define_permission(user,'view', project)
+      visit project_path(project)
+      click_link ticket.title
+      expect(page).to_not have_link("Edit Ticket")
+    end
+    scenario "delete ticket link is shown to a user with permission" do
+      define_permission(user,'view', project)
+      define_permission(user,'delete ticket', project)
+      visit project_path(project)
+      click_link ticket.title
+      expect(page).to have_link("Delete Ticket")
+    end
+    scenario "delete ticket link is not shown to a user with permission" do
+      define_permission(user,'view', project)
+      visit project_path(project)
+      click_link ticket.title
+      expect(page).to_not have_link("Delete Ticket")
+    end
   end
 
   context "admin user" do
@@ -49,6 +87,20 @@ feature "hidden links" do
     scenario "can see the delete project link" do
       visit project_path(project)
       expect(page).to have_link("Delete Project")
+    end
+    scenario "new ticket is shown to admin" do
+      visit project_path(project)
+      expect(page).to have_link("New Ticket")
+    end
+    scenario "edit ticket is shown to admin" do
+      visit project_path(project)
+      click_link ticket.title
+      expect(page).to have_link("Edit Ticket")
+    end
+    scenario "delete ticket is shown to admin" do
+      visit project_path(project)
+      click_link ticket.title
+      expect(page).to have_link("Delete Ticket")
     end
   end
 
